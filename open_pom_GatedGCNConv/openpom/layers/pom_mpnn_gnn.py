c1485028d2f3076821d8459dfa80fb7ebb0645dc
llllhhhhkkkk <63843182+llllhhhhkkkk@users.noreply.github.com>
2025-01-21 14:04:18 +0800

    Add files via upload

---
+++ open_pom_GatedGCNConv/openpom/layers/pom_mpnn_gnn.py
@@ -0,0 +1,86 @@
+import torch.nn as nn
+from dgl.nn.pytorch import GatedGCNConv
+from dgllife.model.gnn import MPNNGNN
+
+
+class CustomMPNNGNN(MPNNGNN):
+    """
+    Customized MPNNGNN layer based MPNNGNN layer in dgllife library.
+
+    Additional options:
+    -> toggle for residual in gnn layer
+    -> choice for message aggregator type
+
+    MPNN is introduced in `Neural Message Passing for Quantum Chemistry
+    <https://arxiv.org/abs/1704.01212>`__.
+
+    This class performs message passing in MPNN
+    and returns the updated node representations.
+    """
+
+    def __init__(self,
+                 node_in_feats: int = 50,
+                 edge_in_feats: int = 50,
+                 node_out_feats: int = 64,
+                 edge_hidden_feats: int = 128,
+                 num_step_message_passing: int = 6,
+                 residual: bool = True,
+                 message_aggregator_type: str = 'sum'):
+        """
+        Parameters
+        ----------
+        node_in_feats: int
+            Size for the input node features.
+        node_out_feats: int
+            Size for the output node representations. Default to 64.
+        edge_in_feats: int
+            Size for the input edge features. Default to 128.
+        edge_hidden_feats: int
+            Size for the hidden edge representations.
+        num_step_message_passing: int
+            Number of message passing steps. Default to 6.
+        residual: bool
+            If true, adds residual layer to gnn layer
+        message_aggregator_type: str
+            message aggregator type, 'sum', 'mean' or 'max'
+        """
+        super(CustomMPNNGNN,
+              self).__init__(node_in_feats=node_in_feats,
+                             edge_in_feats=edge_in_feats,
+                             node_out_feats=node_out_feats,
+                             edge_hidden_feats=edge_hidden_feats,
+                             num_step_message_passing=num_step_message_passing)
+
+        self.gnn_layer = GatedGCNConv(input_feats=node_out_feats,
+                                      output_feats=node_out_feats,
+                                      edge_feats=edge_in_feats
+                                      )
+        self.edge_layer = nn.Linear(node_out_feats, edge_in_feats)
+
+    def forward(self, g, node_feats, edge_feats):
+        """Performs message passing and updates node representations.
+
+        Parameters
+        ----------
+        g : DGLGraph
+            DGLGraph for a batch of graphs.
+        node_feats : float32 tensor of shape (V, node_in_feats)
+            Input node features. V for the number of nodes in the batch of graphs.
+        edge_feats : float32 tensor of shape (E, edge_in_feats)
+            Input edge features. E for the number of edges in the batch of graphs.
+
+        Returns
+        -------
+        node_feats : float32 tensor of shape (V, node_out_feats)
+            Output node representations.
+        """
+        node_feats = self.project_node_feats(node_feats)  # (V, node_out_feats)
+        hidden_feats = node_feats.unsqueeze(0)  # (1, V, node_out_feats)
+
+        for _ in range(self.num_step_message_passing):
+            node_feats, edge_feats = self.gnn_layer(g, node_feats, edge_feats)
+            edge_feats = self.edge_layer(edge_feats)
+            node_feats, hidden_feats = self.gru(node_feats.unsqueeze(0), hidden_feats)
+            node_feats = node_feats.squeeze(0)
+
+        return node_feats
